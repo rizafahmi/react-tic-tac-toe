c53a27c62664060d977c15b18a01b6c47df4fcd3
Riza Fahmi <rizafahmi@gmail.com>
2017-02-22 21:14:16 +0700

    Finish mockup html

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+require('./App.css')
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => {
+  return (
+    <div>
+      <h1>Tic Tac Toe!</h1>
+      <div className='board'>
+        <div className='square' />
+        <div className='square' />
+        <div className='square' />
+        <div className='square' />
+        <div className='square' />
+        <div className='square' />
+        <div className='square' />
+        <div className='square' />
+        <div className='square' />
+        <div className='square' />
+        <div className='square' />
+        <div className='square' />
       </div>
-    );
-  }
+    </div>
+  )
 }
-
-export default App;
+export default App
